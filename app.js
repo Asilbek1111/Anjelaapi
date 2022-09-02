0dbe7b8acc0bbb7e8c7987f32505a0743bbb51a6
Asilbek1111 <99465065+Asilbek1111@users.noreply.github.com>
2022-09-02 15:37:48 +0300

    fix

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-app.set("view engine", "ejs");
+// app.set("view engine", "ejs");
 
 app.use(
   bodyParser.urlencoded({
@@ -123,11 +123,11 @@ app
     });
   });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
+  let port = process.env.PORT;
+  if(port == null || port == ""){
+    port
+  }
 
-app.listen(port, function () {
-  console.log("Server started on port success");
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
 });
